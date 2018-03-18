801d2e668150288716ef68f7999fa931be598927
Kate Torz <ktorz@wp.pl>
2018-03-18 08:58:34 +0100

    change utis

--- src/components/Stact.js
+++ src/components/Stact.js
@@ -1,30 +1,6 @@
 import React, { Component } from 'react';
+import {ratingNames, getStats} from "../utis";
 
-const ratingNames = [
-
-    'Not rated yet',
-
-    'very poor',
-
-    'poor',
-
-    'avg',
-
-    'good',
-
-    'very good',
-]
-
-const getStats = data => data.reduce((prev, current) =>{
-   const ratingName = ratingNames[current.rating]
-   if (prev[ratingName]){
-        prev[ratingName] +=1
-   } else {
-       prev[ratingName] =1
-    }
-    return prev
-
-}, {})
 
 class Statc extends Component {
 
